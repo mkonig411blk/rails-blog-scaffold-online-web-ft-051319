17af886d97a2fd1638f1afbef6e9259f9d60038d
Margot Konig <margot.konig@blackrock.com>
2019-07-23 16:18:47 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
+<<<<<<< HEAD
 class Post < ActiveRecord::Base
+=======
+class Post < ApplicationRecord
+>>>>>>> 3d1708fbbae8dda6e3d20d336c632f3925810739
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2019_07_23_024945) do
+=======
+ActiveRecord::Schema.define(version: 2019_07_23_023959) do
+>>>>>>> 3d1708fbbae8dda6e3d20d336c632f3925810739
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
